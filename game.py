bf1b04b33ab46af7e072819e4b6cde149f29b912
mattmac4241 <mattmac4241@yahoo.com>
2015-09-07 22:39:44 -0500

    Added game file and finished player class

---
+++ game.py
@@ -0,0 +1,9 @@
+from deck import Deck
+from player import Player
+
+'''The game of blackjack, two users(one player the other the dealer)
+if the player goes over 21 he loses, if the player stays under 22 and 
+is greater then the dealer or the dealer goes over 21, then they win. 
+If the dealer and player tie then it's a push and no one loses.'''
+
+
--- player.py
+++ player.py
@@ -5,6 +5,27 @@ also has special type dealer'''
 
 class Player():
     score = 0 #the players hand score
-    deck = [] #contains the hand of the player
+    hand = [] #contains the hand of the player
+    isDealer = False
+
+    #setup player declaring if they are a dealer or not, dealer is the computer and the player is not
+    def __init__(self,isDealer):
+        self.isDealer = isDealer
     
+    #used for the inital hand dealt 
+    def deltHand(self,card1,card2):         
+        self.hand.append(card1)
+        self.hand.append(card2)
+        self.score += card1.getValue() + card2.getValue()
     
+    #delt new card
+    def deltCard(self,card):
+        self.hand.append(card)
+        self.score += card.getValue()
+
+    #return the player's score
+    def getValue(self):
+        return self.score()
+
+
+
